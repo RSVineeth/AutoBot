9cd14de67547e9fd4a602a88257870346ac85419
RSVineeth <rshivavineeth@gmail>
2025-04-26 12:44:38 +0530

    uptimebot

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, Response
 import os
 import threading
 from trading_loop import main  # import the function
+import time
+from datetime import datetime  
 
 
 varport = int(os.getenv("PORT", 5000))
@@ -10,6 +12,12 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'HEAD'])
 def home():
     return Response("Bot is running!", status=200)
+
+@app.route('/uptime', methods=['GET'])
+def uptime():
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return Response(f"Bot uptime check: {now}", status=200)
+
 
 def start_bot():
     print("🚀 Starting trading_loop thread")
